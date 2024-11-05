224e8ac787d58478bb8d30f78712e5256e01122a
Kelvin Charles <kelvinc1291@hotmail.com>
2024-11-05 16:08:38 -0300

    feat(styles): add responsive in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,35 +6,46 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 const LoginPage = async () => {
-    const { userId } = await auth()
+  const { userId } = await auth();
 
-    if (userId) {
-        redirect('/')
-    }
+  if (userId) {
+    redirect("/");
+  }
 
-    return (
-        <div className="grid grid-cols-2 h-full">
-            <div className="flex flex-col h-full justify-center p-8 max-w-[550px] mx-auto">
-                <Image src={'/logo.svg'} alt="Finance AI" width={179} height={39} className="mb-8" />
-                <h1 className="text-4xl font-bold mb-3">Bem-vindo</h1>
-                <p className="text-muted-foreground mb-8">
-                    A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.
-                </p>
-                <SignInButton>
-                    <Button variant='outline' >
-                        <LogInIcon className="mr-2" />
-                        Fazer login ou criar conta
-                    </Button>
-                </SignInButton>
+  return (
+    <div className="mx-auto flex h-full max-w-[1440px] md:grid md:grid-cols-2">
+      <div className="flex h-full w-full flex-col items-center justify-center p-8 md:mx-auto md:max-w-[550px] md:items-stretch">
+        <Image
+          src={"/logo.svg"}
+          alt="Finance AI"
+          width={179}
+          height={39}
+          className="mb-8"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
+        <p className="mb-8 text-center text-muted-foreground md:text-start">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
+        </p>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
+      </div>
 
-            </div>
-
-            <div className="relative h-full w-full">
-                <Image src={'/login.png'} alt="Faça login" fill className="object-cover" />
-            </div>
-
-        </div>
-    );
-}
+      <div className="relative hidden h-full w-full md:block">
+        <Image
+          src={"/login.png"}
+          alt="Faça login"
+          fill
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default LoginPage;
