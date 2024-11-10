f79186d3adfa1645b689d2ac5084e1f7339a2535
Kelvin Charles <kelvinc1291@hotmail.com>
2024-11-10 20:35:35 -0300

    feat: add last component

---
+++ app/(home)/_components/last-transactions.tsx
@@ -0,0 +1,75 @@
+import { TRANSACTION_PAYMENT_METHOD_ICONS } from "@/app/_components/_constants/transactions";
+import { Button } from "@/app/_components/ui/button";
+import { CardContent, CardHeader, CardTitle } from "@/app/_components/ui/card";
+import { ScrollArea } from "@/app/_components/ui/scroll-area";
+import { formatCurrency } from "@/app/utils/currency";
+import { Transaction, TransactionType } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+
+interface LastTransactionsProps {
+  lastTransactions: Transaction[];
+}
+
+const LastTransactions = ({ lastTransactions }: LastTransactionsProps) => {
+  const getAmountPrefix = (transaction: Transaction) => {
+    if (transaction.type === TransactionType.DEPOSIT) return "+";
+    return "-";
+  };
+
+  const getAmountColor = (transaction: Transaction) => {
+    if (transaction.type === TransactionType.EXPENSE) return "text-red-500";
+    else if (transaction.type === TransactionType.DEPOSIT)
+      return "text-primary";
+    else return "text-white";
+  };
+  return (
+    <ScrollArea className="rounded-md border">
+      <CardHeader className="flex-row items-center justify-between">
+        <CardTitle className="font-bold">Últimas Transações</CardTitle>
+        <Button variant="outline" className="rounded-full font-bold" asChild>
+          <Link href={"/transactions"}>Ver mais</Link>
+        </Button>
+      </CardHeader>
+
+      <CardContent className="space-y-6">
+        {lastTransactions.map((transaction) => (
+          <div
+            key={transaction.id}
+            className="flex items-center justify-between"
+          >
+            <div className="flex items-center gap-3">
+              <div className="rounded-lg bg-white bg-opacity-[3%] p-3">
+                <Image
+                  src={
+                    TRANSACTION_PAYMENT_METHOD_ICONS[transaction.paymentMethod]
+                  }
+                  height={20}
+                  width={20}
+                  alt="pix"
+                />
+              </div>
+              <div>
+                <p className="text-sm font-bold">{transaction.name}</p>
+                <p className="text-sm text-muted-foreground">
+                  {new Date(transaction.date).toLocaleDateString("pt-BR", {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric",
+                  })}
+                </p>
+              </div>
+            </div>
+
+            <p className={`text-sm font-bold ${getAmountColor(transaction)}`}>
+              {getAmountPrefix(transaction)}
+              {formatCurrency(Number(transaction.amount))}
+            </p>
+          </div>
+        ))}
+      </CardContent>
+    </ScrollArea>
+  );
+};
+
+export default LastTransactions;
